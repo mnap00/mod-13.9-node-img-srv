9d9a218a570894b735f4d26fb743bd1f555a678c
mnap <learn@10g.pl>
2018-04-02 17:16:17 +0200

    Fix 'cross-device link not permitted' issue

--- modules/handlers.js
+++ modules/handlers.js
@@ -4,11 +4,21 @@ var formidable = require('formidable');
 exports.upload = function(request, response) {
     console.log('Handling upload request...');
     var form = new formidable.IncomingForm();
+    form.uploadDir = './temporary'
     form.parse(request, function(error, fields, files) {
+        console.log(fields);
         fs.renameSync(files.upload.path, 'test.png');
-        response.writeHead(200, {'Content-Type': 'text-html'});
+        response.writeHead(200, {'Content-Type': 'text/html'});
         response.write('Received image:<br/>');
         response.write('<img src="/show" />');
+        response.end();
+    });
+}
+
+exports.show = function(request, response) {
+    fs.readFile('test.png', 'binary', function(error, file) {
+        response.writeHead(200, {'Content-Type': 'image/png'});
+        response.write(file, 'binary');
         response.end();
     });
 }
