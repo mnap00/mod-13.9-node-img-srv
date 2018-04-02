f1003154237a38e3fdf97b0d3826b36aa087f3bc
mnap <learn@10g.pl>
2018-04-02 10:48:33 +0200

    Add send file handler and formidable module

--- modules/handlers.js
+++ modules/handlers.js
@@ -1,9 +1,16 @@
 var fs = require('fs');
+var formidable = require('formidable');
 
 exports.upload = function(request, response) {
     console.log('Handling upload request...');
-    response.write('Begin upload!');
-    response.end();
+    var form = new formidable.IncomingForm();
+    form.parse(request, function(error, fields, files) {
+        fs.renameSync(files.upload.path, 'test.png');
+        response.writeHead(200, {'Content-Type': 'text-html'});
+        response.write('Received image:<br/>');
+        response.write('<img src="/show" />');
+        response.end();
+    });
 }
 
 exports.welcome = function(request, response) {
